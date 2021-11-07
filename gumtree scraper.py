9fb740f0ba0f42d7a5c7a66bad8f2ab2d350c1e8
Toby Cook <cootob@gmail.com>
2021-11-07 14:58:45 +0000

    initial commit

---
+++ gumtree scraper.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+
+
+link = "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fhealthandsocialcare%2fconditionsanddiseases%2fdatasets%2fcoronaviruscovid19infectionsurveydata%2f2021/covid19infectionsurveydatasets20211105england.xlsx"
+
+
